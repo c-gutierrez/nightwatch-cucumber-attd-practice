a67f785d5a8db77b21efd1e298438095222a832b
Carlos Gutiérrez <carlos.gutierrez@gigigo.com.mx>
2018-03-06 00:27:31 -0600

    Nightwatch-Cucumber implemented test

--- features/step_definitions/register.js
+++ features/step_definitions/register.js
@@ -2,49 +2,69 @@ const { client } = require('nightwatch-cucumber');
 const { Given, Then, When } = require('cucumber');
 
 Given('I am on the home screen', () => {
-    return Promise.reject('Test fail!');
+    return client
+        .url('http://localhost:8080')
+        .waitForElementVisible('#title', 5000)
+        .assert
+        .containsText('#title', 'ATDD');
 });
 
 Given('I am not registered', () => {
-    return Promise.reject('Test fail!');
+    return client
+        .assert
+        .containsText('h1', 'Home Page');
 });
 
 Given('I see the Register button', () => {
-    return Promise.reject('Test fail!');
+    return client
+        .assert
+        .elementPresent('#registerButton');
 });
 
 When('I press the Register button', () => {
-    return Promise.reject('Test fail!');
+    return client
+        .click('#registerButton');
 });
 
 Then('I should see the Register form', () => {
-    return Promise.reject('Test fail!');
+    return client
+        .assert
+        .elementPresent('#registerForm');
 });
 
 Given('I fill name input with {string}', (name) => {
-    return Promise.reject('Test fail!');
+    return client
+        .setValue('#inputName', name);
 });
 
 Given('lastname input with {string}', (lastname) => {
-    return Promise.reject('Test fail!');
+    return client
+        .setValue('input[name="lastname"]', lastname);
 });
 
 Given('email input with {string}', (email) => {
-    return Promise.reject('Test fail!');
+    return client
+        .setValue('input[name="email"]', email);
 });
 
-Given('pass input with {string}', (email) => {
-    return Promise.reject('Test fail!');
+Given('pass input with {string}', (pass) => {
+    return client
+        .setValue('input[name="pass"]', pass);
 });
 
 Given('I press the submit button', () => {
-    return Promise.reject('Test fail!');
+    return client
+        .click('#submit');
 });
 
 Then('I go to the welcome screen', () => {
-    return Promise.reject('Test fail!');
+    return client
+        .assert
+        .containsText('h1', 'Welcome Page');
 });
 
 Then('the site should shows {string}', (message) => {
-    return Promise.reject('Test fail!');
+    return client
+        .assert
+        .containsText('h2', message);
 });
